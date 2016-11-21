01dc8002c0c104f048d642b8901b0e81bac2239c
Marta Panuszewska <mpanuszewska@gmail.com>
2016-11-21 12:32:30 +0100

    dsa

--- availability.py
+++ availability.py
@@ -13,11 +13,15 @@ class Availabilitytests(unittest.TestCase):
 		pp = pprint.PrettyPrinter(indent=4)
 
 		self.docker = Client()
+		#self.docker.start(u'riak01')
 
 		zomg = self.docker.exec_create(container=u'riak01', cmd="riak-admin status | grep ring")
 		print self.docker.exec_start(zomg)
 
-		myClient = riak.RiakClient(pb_port=12101, protocol='pbc')
+		zomg = self.docker.exec_create(container=u'riak01', cmd="riak-admin bucket-type create vectors '{\"props\":{\"allow_mult\": true, \"n_val\":3}}'")
+		print self.docker.exec_start(zomg)
+
+		myClient = riak.RiakClient(pb_port=12103, protocol='pbc')
 		self.myBucket = myClient.bucket('test')
 
 	def test_Read_fail_Read(self):
@@ -87,6 +91,50 @@ class Availabilitytests(unittest.TestCase):
 		self.assertTrue( "two" == fetched2.data)
 		self.assertTrue("three" == fetched3.data)
 		self.assertTrue("four" == fetched4.data)
+
+	def test_Read_even_during_failure(self):
+		key1 = self.myBucket.new('data', "stored")
+		key1.store()
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+
+		sleep(10)
+		self.docker.stop(u'riak01')
+		sleep(10)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+		self.docker.start(u'riak01')
+		sleep(30)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+
+		self.docker.stop(u'riak02')
+		sleep(10)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+		self.docker.start(u'riak02')
+		sleep(30)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+
+		#piszemy do tego node'a bezposrednio, wiec nie pojdzie tutaj :)
+		self.docker.stop(u'riak03')
+		sleep(10)
+		with self.assertRaises(Exception):
+			self.myBucket.get('data', r=2).data
+		self.docker.start(u'riak03')
+		sleep(30)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+
+		self.docker.stop(u'riak04')
+		sleep(10)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+		self.docker.start(u'riak04')
+		sleep(30)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+
+		self.docker.stop(u'riak05')
+		sleep(10)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+		self.docker.start(u'riak05')
+		sleep(30)
+		self.assertTrue("stored" == self.myBucket.get('data', r=2).data)
+
 
 
 def main():
